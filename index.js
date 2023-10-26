531957d20a295528a8d0fecb0d99105602642d46
KevinJoseph <kevramos95@gmail.com>
2023-10-26 18:58:32 -0500

    ejemplo

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+
+app.get('/thanks', (req, res) => {
+  res.send('Universidad Nacional de Cañete')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
